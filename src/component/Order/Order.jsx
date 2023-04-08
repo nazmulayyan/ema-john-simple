ed1fa6a72a7277cc8e96ac703fbcf28c746ca102
NazmulAyyan <rnazmul2424@gmail.com>
2023-04-09 00:29:29 +0600

    added products in local stored

--- src/component/Order/Order.jsx
+++ src/component/Order/Order.jsx
@@ -3,25 +3,63 @@ import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../products/Product';
 import './Order.css'
+
 const product = () => {
     const [products, setProducts] = useState([]);
     //cart handler
     const [cart, setCart] = useState([]);
 
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[]);
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
 
-    useEffect(()=>{
+    useEffect(() =>{
         const storedCart = getShoppingCart();
-        console.log(storedCart)
-    },[]);
+        const savedCart = [];
+        //step 1: get id of the addedProduct
+        for(const id in storedCart){
+            //step 2: get product from products state by using id
+            const addedProduct = products.find(product =>product.id === id)
+            if(addedProduct){
+                //step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity= quantity;
+                // step 4: add the added product to the saved cart
+                savedCart.push(addedProduct)
+            }
+            console.log('added product', addedProduct)
+        }
+        //step 5: set the cart
+        setCart(savedCart);
+    }, [products])
+
+    // useEffect(() => {
+    //     const storedCart = getShoppingCart();
+    //     //step 01 get id
+    //     for (const id in storedCart) {
+    //         // console.log(id)
+    //         //set 02: get the product by using id
+    //         const addedProduct = products.find(product => product.id === id);
+
+    //         //step 3: get quantity of the product.
+    //         if (addedProduct) {
+    //             const quantity = storedCart[id];
+    //             addedProduct.quantity = quantity
+    //             console.log(addedProduct);
+    //         }
+            
+    //         // const quantity = storedCart[id];
+    //         // console.log(quantity);
+    //         // addedProduct.quantity = quantity
+    //         // console.log(addedProduct);
+    //     }
+    // }, [products]);
 
     //add to cart handler
-    const handleAddToCart = (product) =>{
+    const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
         addToDb(product.id);
@@ -32,9 +70,9 @@ const product = () => {
             <div className="shop-container">
                 {
                     products.map(product => <Product
-                    key= {product.id}
-                    product= {product}
-                    handleAddToCart= {handleAddToCart}
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
